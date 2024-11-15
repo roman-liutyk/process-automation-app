59502c58dfced70e94b1e9abf1bfd57340092d96
roman-liutyk <lytikroman5@gmail.com>
2024-11-15 10:53:24 +0100

    feat: added some routes to work with

--- lib/common/router/router.dart
+++ lib/common/router/router.dart
@@ -1,13 +1,27 @@
 import 'package:go_router/go_router.dart';
 import 'package:process_automation_app/features/auth/views/sign_in_view.dart';
+import 'package:process_automation_app/features/auth/views/sign_up_view.dart';
+import 'package:process_automation_app/features/dashboard/views/dashboard_view.dart.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/sign_in',
+  initialLocation: '/',
   routes: [
     GoRoute(
       path: '/sign_in',
       pageBuilder: (context, state) => const NoTransitionPage(
         child: SignInView(),
+      ),
+    ),
+    GoRoute(
+      path: '/sign_up',
+      pageBuilder: (context, state) => const NoTransitionPage(
+        child: SignUpView(),
+      ),
+    ),
+    GoRoute(
+      path: '/',
+      pageBuilder: (context, state) => const NoTransitionPage(
+        child: DashboardView(),
       ),
     ),
   ],
