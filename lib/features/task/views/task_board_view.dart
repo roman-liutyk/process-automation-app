d986e9d690735f0f51f5deff7df98cb9fdc226f7
vlad lekomtsev <lvlad3770@gmail.com>
2024-11-17 22:17:42 +0200

    feat: improve task board layout and enhance drag-and-drop functionality

--- lib/features/task/views/task_board_view.dart
+++ lib/features/task/views/task_board_view.dart
@@ -23,88 +23,85 @@ class TaskBoardView extends ConsumerWidget {
           state.map(
             loaded: (loaded) {
               return Expanded(
-                child: ListView.separated(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 24,
-                    horizontal: 36,
-                  ),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    final tasks = loaded.tasks
-                        .where((e) => e.status == TaskStatusEnum.values[index])
-                        .toList();
+                child: Center(
+                  child: ListView.separated(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 24,
+                      horizontal: 36,
+                    ),
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final tasks = loaded.tasks.where((e) => e.status == TaskStatusEnum.values[index]).toList();
 
-                    return DragTarget<String>(
-                      onAcceptWithDetails: (data) {
-                        ref.read(taskProvider.notifier).updateStatus(
-                              updatedTask: loaded.tasks
-                                  .firstWhere((e) => e.id == data.data)
-                                  .copyWith(
-                                    status: TaskStatusEnum.values[index],
-                                  ),
-                            );
-                      },
-                      builder: (context, candidateData, rejectedData) {
-                        return DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: TaskStatusEnum.values[index].backgroundColor,
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(12),
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.25),
-                                blurRadius: 10,
-                                offset: const Offset(0, 4),
-                              ),
-                            ],
-                          ),
-                          child: SizedBox(
-                            width: 300,
-                            child: Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    TaskStatusEnum.values[index].title,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 18,
+                      return DragTarget<String>(
+                        onAcceptWithDetails: (data) {
+                          ref.read(taskProvider.notifier).updateStatus(
+                                updatedTask: loaded.tasks.firstWhere((e) => e.id == data.data).copyWith(
+                                      status: TaskStatusEnum.values[index],
                                     ),
-                                  ),
-                                  const SizedBox(
-                                    height: 8,
-                                  ),
-                                  Expanded(
-                                    child: ListView.separated(
-                                      itemBuilder: (context, index) {
-                                        return TaskListItem(
-                                          task: tasks[index],
-                                        );
-                                      },
-                                      separatorBuilder: (context, index) =>
-                                          const SizedBox(
-                                        height: 16,
+                              );
+                        },
+                        builder: (context, candidateData, rejectedData) {
+                          return DecoratedBox(
+                            decoration: BoxDecoration(
+                              color: TaskStatusEnum.values[index].backgroundColor,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(12),
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.25),
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 4),
+                                ),
+                              ],
+                            ),
+                            child: SizedBox(
+                              width: 300,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      TaskStatusEnum.values[index].title,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 18,
                                       ),
-                                      itemCount: tasks.length,
                                     ),
-                                  ),
-                                ],
+                                    const SizedBox(
+                                      height: 8,
+                                    ),
+                                    Expanded(
+                                      child: ListView.separated(
+                                        itemBuilder: (context, index) {
+                                          return TaskListItem(
+                                            task: tasks[index],
+                                          );
+                                        },
+                                        separatorBuilder: (context, index) => const SizedBox(
+                                          height: 16,
+                                        ),
+                                        itemCount: tasks.length,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                  separatorBuilder: (context, index) {
-                    return const SizedBox(
-                      width: 20,
-                    );
-                  },
-                  itemCount: TaskStatusEnum.values.length,
+                          );
+                        },
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(
+                        width: 20,
+                      );
+                    },
+                    itemCount: TaskStatusEnum.values.length,
+                  ),
                 ),
               );
             },
